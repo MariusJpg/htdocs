ac1b415bae5ad3afafa74ea613408afc856ba4f7
MariusJpg <mayoos2210@gmail.com>
2021-08-26 13:00:22 +0200

    >Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MariusJB</title>
+</head>
+<body>
+    <h1>Individet Marius </h1>
+
+    
+
+    <img src="img/Ludvig-xiv.jpg " alt="Ludvig-xiv.jpg">
+
+    <p> Marius er 169 høy, Marius er veldig sterk, Marius liker oliven,</p>
+    <p> Hvis du vil ha Marius. Kontakt Marius 3Daa G280 Malakoff vgs. </p>
+
+    <h2><a href="oblig2/oblig2.php">oblig2</a></h2>
+    <h3><a href="oblig2/oppg2.php">oppg2</a></h3>
+    <a href="https://soundcloud.com/user-843295696">soundcloud</a>
+
+</body>
+</html>
---
+++ oblig2/oppg2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MariusJB2</title>
+</head>
+<body>
+    
+    <li>Fjernsynskanaler
+        <ol>
+            <li><img src="img/kommentar.png " alt="kommentar.png"></li> <!-- kommentar-->
+                Nei liker den ikke,     
+           
+            <li></li>
+        </lo>
+    </li>
+</ul>
+
+</body>
+</html>
