55a4f09acf115058051212a5ab14e4fba7623408
MariusJpg <mayoos2210@gmail.com>
2022-04-05 12:54:30 +0200

    04.05.2022

--- bosy/selectconnect.php
+++ bosy/selectconnect.php
@@ -8,28 +8,29 @@
     
   
   }  
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "busy";
+  
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
   }
-} else {
-  echo "0 results";
-}
-$conn->close();
+  
+  $sql = "SELECT Navn, Orgnr, Adresse FROM Firmaer WHERE Orgnr='$selectorgnr' OR Navn='$selectnavn' OR Adresse='$selectadresse'";
+
+  $result = $conn->query($sql);
+  
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "Navn: " . $row["Navn"]. " - Orgnr: " . $row["Orgnr"]. "-Adresse" . $row["Adresse"]. "<br>";
+    }
+  } else {
+    echo "0 results";
+  }
+  $conn->close();
 ?>
